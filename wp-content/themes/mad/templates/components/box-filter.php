6afddebc79187a2d304cd721f1d6217fed7b194b
= <hung0895@gmail.com>
2019-02-14 23:43:07 +0700

    mad: fix bug

--- wp-content/themes/mad/templates/components/box-filter.php
+++ wp-content/themes/mad/templates/components/box-filter.php
@@ -8,18 +8,30 @@
             'taxonomy' => 'product_filter',
             'hide_empty' => false
         ));
+
+        // get url current no pagination
+        global $wp;
+
+        // get current url with query string.
+        $current_url =  home_url( $wp->request ); 
+
+        // get the position where '/page.. ' text start.
+        $pos = strpos($current_url , '/page/');
+
+        // remove string from the specific postion
+        $finalurl = substr($current_url,0,$pos);
       ?>
       <?php foreach ($style_terms as $key => $style_term): ?>
         <?php if (get_query_var('style', 'all') == $style_term->slug): ?>
-          <li class="box-filter__item"><a href="<?php print add_query_arg('style', $style_term->slug); ?>#style-position" class="btn is-active"><?php print $style_term->name; ?></a></li>
+          <li class="box-filter__item"><a href="<?php echo add_query_arg('style', $style_term->slug, $finalurl); ?>#style-position" class="btn is-active"><?php echo $style_term->name; ?></a></li>
         <?php else: ?>
-          <li class="box-filter__item"><a href="<?php print add_query_arg('style', $style_term->slug); ?>#style-position" class="btn"><?php print $style_term->name; ?></a></li>
+          <li class="box-filter__item"><a href="<?php echo add_query_arg('style', $style_term->slug, $finalurl); ?>#style-position" class="btn"><?php echo $style_term->name; ?></a></li>
         <?php endif ?>
       <?php endforeach ?>
         <?php if (get_query_var('style', 'all') == 'all'): ?>
-          <li class="box-filter__item"><a href="<?php print remove_query_arg('style'); ?>#style-position" class="btn is-active">VIEW ALL</a></li>
+          <li class="box-filter__item"><a href="<?php echo remove_query_arg('style'); ?>#style-position" class="btn is-active">VIEW ALL</a></li>
         <?php else: ?>
-          <li class="box-filter__item"><a href="<?php print remove_query_arg('style'); ?>#style-position" class="btn">VIEW ALL</a></li>
+          <li class="box-filter__item"><a href="<?php echo remove_query_arg('style'); ?>#style-position" class="btn">VIEW ALL</a></li>
         <?php endif ?>
     </ul>
   </div>
